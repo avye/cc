8921235be9462be4defb08a7c19fc31935912978
Andrew Vye <a.vye26@gmail.com>
2016-10-21 10:51:59 -0700

    (test) Ensure that the deduplicate function returns an array

--- code/spec/spec.js
+++ code/spec/spec.js
@@ -6,4 +6,8 @@ describe('Test Suite', () => {
   it('should have a deduplicate function', () => {
     expect(code.deduplicate).to.be.a('function');
   })
+
+  it('should return an array', () => {
+    expect(code.deduplicate([])).to.be.an('array');
+  })
 })
