ab4de144a550cd11a18549cf4bcbabf88b743be0
Andrew Vye <a.vye26@gmail.com>
2016-10-21 12:33:30 -0700

    (feat) Update deduplicate to prioritize by date

--- code/index.js
+++ code/index.js
@@ -7,13 +7,17 @@ export const deduplicate = (inputArray) => {
 
   inputArray.forEach((element) => {
     if (ids[element._id]) {
-      ids[element._id] = element;
-      emails[element.email] = element;
+      if (new Date(ids[element._id].entryDate) <= new Date(element.entryDate)) {
+        ids[element._id] = element;
+        emails[element.email] = element;
+      }
     } else if (emails[element.email]) {
-      // since ids is our main source of truth, remove and update the record there
-      delete ids[emails[element.email]._id]
-      ids[element._id] = element;
-      emails[element.email] = element;
+      if (new Date(emails[element.email].entryDate) <= new Date(element.entryDate)) {
+        // since ids is our main source of truth, remove and update the record there
+        delete ids[emails[element.email]._id]
+        ids[element._id] = element;
+        emails[element.email] = element;
+      }
     } else {
       ids[element._id] = element;
       emails[element.email] = element;
