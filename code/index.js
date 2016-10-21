007c86b6bd705037167e2e89bb8d9480e259d0cd
Andrew Vye <a.vye26@gmail.com>
2016-10-21 11:26:52 -0700

    (feat) Update deduplicate to handle duplicate ids

--- code/index.js
+++ code/index.js
@@ -1,5 +1,18 @@
 'use strict'
 
-export const deduplicate = () => {
-  return [];
+export const deduplicate = (inputArray) => {
+  let ids = {};
+
+  inputArray.forEach((element) => {
+    if (ids[element._id]) {
+      ids[element._id] = element;
+    } else {
+      ids[element._id] = element;
+    }
+  })
+
+  return Object.keys(ids).reduce((acc, el) => {
+    acc.push(ids[el]);
+    return acc;
+  }, [])
 }
