8a4697e89be86cfe0246600d8044754427cf8a4e
Andrew Vye <a.vye26@gmail.com>
2016-10-21 12:20:06 -0700

    (feat) Update deduplicate to handle duplicate emails

--- code/index.js
+++ code/index.js
@@ -1,13 +1,22 @@
 'use strict'
 
 export const deduplicate = (inputArray) => {
+  // ids will be our main source of truth
   let ids = {};
+  let emails = {};
 
   inputArray.forEach((element) => {
     if (ids[element._id]) {
       ids[element._id] = element;
+      emails[element.email] = element;
+    } else if (emails[element.email]) {
+      // since ids is our main source of truth, remove and update the record there
+      delete ids[emails[element.email]._id]
+      ids[element._id] = element;
+      emails[element.email] = element;
     } else {
       ids[element._id] = element;
+      emails[element.email] = element;
     }
   })
 
