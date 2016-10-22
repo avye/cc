03fa48956c13decbdcfa16542cbd7b4e18e631b2
Andrew Vye <a.vye26@gmail.com>
2016-10-22 15:45:40 -0700

    (chore) Add legend to the log to better explain the changes

--- code/makeLog.js
+++ code/makeLog.js
@@ -15,5 +15,7 @@ export const makeLog = (filename, inputData, outputData) => {
   fs.appendFileSync(updatedFilename, JSON.stringify(outputData, null, '\t'));
 
   fs.appendFileSync(updatedFilename, '\n # Differences between the two \n');
+  let legend = 'The kind indicates the kind of change and can be:\nN - indicates a newly added property/element\nD - indicates a property/element was deleted\nE - indicates a property/element was edited\nA - indicates a change occurred within an array\nlhs represents the original and rhs represents the new output\n'
+  fs.appendFileSync(updatedFilename, legend);
   fs.appendFileSync(updatedFilename, JSON.stringify(difference, null, '\t'));
 }
