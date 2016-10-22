3420a0cde9c5241b30771bcf5024a9c92bedf04f
Andrew Vye <a.vye26@gmail.com>
2016-10-22 13:16:27 -0700

    (feat) Update makeLog to handle two sets of data

--- code/makeLog.js
+++ code/makeLog.js
@@ -1,10 +1,11 @@
 'use strict'
 import * as fs from 'fs';
 
-export const makeLog = (filename, data) => {
+export const makeLog = (filename, inputData, outputData) => {
   let lastPeriodIndex = filename.lastIndexOf('.');
   // Add the word Log to the filename
   let updatedFilename = filename.slice(0, lastPeriodIndex).concat('Log.md');
 
-  fs.appendFileSync(updatedFilename, data);
+  fs.appendFileSync(updatedFilename, inputData);
+  fs.appendFileSync(updatedFilename, outputData);
 }
