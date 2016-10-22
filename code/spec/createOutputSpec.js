9e9b6200200b64d0d787c10781f6f088a8ba7d81
Andrew Vye <a.vye26@gmail.com>
2016-10-21 19:03:38 -0700

    (test) Ensure that createOutput writes a file

--- code/spec/createOutputSpec.js
+++ code/spec/createOutputSpec.js
@@ -1,9 +1,27 @@
 'use strict'
 import { expect } from 'chai';
 import { createOutput } from '../createOutput';
+import * as fs from 'fs';
 
 describe('CreateOutput Test Suite', () => {
+  before(() => {
+    // Check to see if the file exists already
+    fs.access('./code/spec/specData/test.json', (err) => {
+      if (!err) {
+        // Delete it if it does
+        fs.unlink('./code/spec/specData/test.json')
+      }
+    })
+  })
+
   it('should have a createOutput function', () => {
     expect(createOutput).to.be.a('function');
+  })
+
+  it('should create a file', () => {
+    createOutput();
+    fs.access('./code/spec/specData/test.json', (err) => {
+      expect(err).to.be.(null);
+    })
   })
 })
