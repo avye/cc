634be4ee11edd924da514dffda4c6c052fa994a7
Andrew Vye <a.vye26@gmail.com>
2016-10-22 13:41:38 -0700

    (feat) Update makeLog to better display data and differences as well as bring in deep-diff

--- code/makeLog.js
+++ code/makeLog.js
@@ -1,11 +1,19 @@
 'use strict'
 import * as fs from 'fs';
+import { diff } from 'deep-diff';
 
 export const makeLog = (filename, inputData, outputData) => {
   let lastPeriodIndex = filename.lastIndexOf('.');
   // Add the word Log to the filename
   let updatedFilename = filename.slice(0, lastPeriodIndex).concat('Log.md');
+  let difference = diff(inputData, outputData);
 
-  fs.appendFileSync(updatedFilename, inputData);
-  fs.appendFileSync(updatedFilename, outputData);
+  fs.appendFileSync(updatedFilename, '# Original Source Data \n');
+  fs.appendFileSync(updatedFilename, JSON.stringify(inputData, null, '\t'));
+
+  fs.appendFileSync(updatedFilename, '\n # Output Data \n');
+  fs.appendFileSync(updatedFilename, JSON.stringify(outputData, null, '\t'));
+
+  fs.appendFileSync(updatedFilename, '\n # Differences between the two \n');
+  fs.appendFileSync(updatedFilename, JSON.stringify(difference, null, '\t'));
 }
