b6015dd30970f674886374f8f7a522648bed8b80
Andrew Vye <a.vye26@gmail.com>
2016-10-22 13:10:07 -0700

    (feat) Update makeLog to output a markdown file

--- code/makeLog.js
+++ code/makeLog.js
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 export const makeLog = (filename, data) => {
   let lastPeriodIndex = filename.lastIndexOf('.');
   // Add the word Log to the filename
-  let updatedFilename = filename.slice(0, lastPeriodIndex).concat('Log', filename.slice(lastPeriodIndex));
+  let updatedFilename = filename.slice(0, lastPeriodIndex).concat('Log.md');
 
   fs.appendFileSync(updatedFilename, data);
 }
