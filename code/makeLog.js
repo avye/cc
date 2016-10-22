8d0df35916f59394b9a7167865d107a90a86cc55
Andrew Vye <a.vye26@gmail.com>
2016-10-22 12:26:44 -0700

    (feat) Update makeLog to add Log to the title given a filename

--- code/makeLog.js
+++ code/makeLog.js
@@ -1,6 +1,10 @@
 'use strict'
 import * as fs from 'fs';
 
-export const makeLog = () => {
-  fs.appendFileSync('./code/spec/specData/testLog.json', 'test')
+export const makeLog = (filename) => {
+  let lastPeriodIndex = filename.lastIndexOf('.')
+  // Add the word Log to the filename
+  let updatedFilename = filename.slice(0, lastPeriodIndex).concat('Log', filename.slice(lastPeriodIndex))
+
+  fs.appendFileSync(updatedFilename, 'test')
 }
