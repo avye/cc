939f02b54575fd2be09bd3203b5385168330ccf3
Andrew Vye <a.vye26@gmail.com>
2016-10-22 12:45:33 -0700

    (feat) Update makeLog to receive data and add it to file

--- code/makeLog.js
+++ code/makeLog.js
@@ -1,10 +1,10 @@
 'use strict'
 import * as fs from 'fs';
 
-export const makeLog = (filename) => {
-  let lastPeriodIndex = filename.lastIndexOf('.')
+export const makeLog = (filename, data) => {
+  let lastPeriodIndex = filename.lastIndexOf('.');
   // Add the word Log to the filename
-  let updatedFilename = filename.slice(0, lastPeriodIndex).concat('Log', filename.slice(lastPeriodIndex))
+  let updatedFilename = filename.slice(0, lastPeriodIndex).concat('Log', filename.slice(lastPeriodIndex));
 
-  fs.appendFileSync(updatedFilename, 'test')
+  fs.appendFileSync(updatedFilename, data);
 }
