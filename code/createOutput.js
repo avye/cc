87304630fb655eb73fd86e8a4e858fe4aea25aac
Andrew Vye <a.vye26@gmail.com>
2016-10-22 13:33:46 -0700

    (fix) Add some spacing to pretty-print the json

--- code/createOutput.js
+++ code/createOutput.js
@@ -2,6 +2,6 @@
 import * as fs from 'fs';
 
 export const createOutput = (filename, data) => {
-  let JSONdata = JSON.stringify(data);
+  let JSONdata = JSON.stringify(data, null, '\t');
   fs.appendFileSync(filename, JSONdata)
 }
