e2bb74182477c968e7c3d76618fae9cfa80d27b2
Andrew Vye <a.vye26@gmail.com>
2016-10-22 12:13:05 -0700

    (test) Ensure that makeLog creates a file

--- code/spec/makeLogSpec.js
+++ code/spec/makeLogSpec.js
@@ -1,9 +1,17 @@
 'use strict'
 import { expect } from 'chai';
 import { makeLog } from '../makeLog';
+import * as fs from 'fs';
 
 describe('MakeLog Test Suite', () => {
   it('should have a makeLog function', () => {
     expect(makeLog).to.be.a('function');
   })
+
+  it('should create a file', () => {
+    makeLog();
+    let exists = fs.existsSync('./code/spec/specData/testLog.json');
+    expect(exists).to.be.true;
+  })
+
 })
